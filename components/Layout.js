67f7ece22fbb1d4f38b818612e46e33e4d9600aa
dentemple <denny.temple@gmail.com>
2017-03-18 16:57:02 -0400

    Apply a baseline styling to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+const LayoutWrapper = (props) => {
+  const style = {
+    maxWidth: 650,
+    margin: '40px auto',
+    lineHeight: 1.6,
+    fontSize: 18,
+    color: '#333333',
+    padding: '0 10px'
+  }
+  return (
+    <div style={style}>
+      { props.children }
+    </div>
+  )
+}
+
 const Layout = ({children}) => (
-  <div>
+  <LayoutWrapper>
     { children }
-  </div>
+  </LayoutWrapper>
 );
 
 export default Layout;
