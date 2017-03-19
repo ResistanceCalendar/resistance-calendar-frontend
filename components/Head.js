e3d16c4b75c7040edf5f10b1fd755f6f3fe2450f
dentemple <denny.temple@gmail.com>
2017-03-19 12:42:00 -0400

    (Temporarily) Add the font-awesome CDN

--- components/Head.js
+++ components/Head.js
@@ -9,6 +9,7 @@ export default class extends React.Component {
         <link rel='manifest' href='static/manifest.json' />
         <link rel='icon' sizes='32x32' type='image/png' href='static/favicon.ico' />
         <link href='https://fonts.googleapis.com/css?family=Open+Sans|Roboto' rel='stylesheet' />
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
         <title>Resistance Calendar</title>
       </Head>
     );
