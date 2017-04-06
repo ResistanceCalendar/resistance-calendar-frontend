fd9193cd51b9e96a26c3da480a3df7c42b641fae
sdotson <stu.dotson@gmail.com>
2017-04-06 16:57:57 -0400

    abstract out fb sdk, fix lint errors

--- src/components/SocialBtns/SocialBtns.js
+++ src/components/SocialBtns/SocialBtns.js
@@ -1,30 +1,50 @@
-import React, { PropTypes } from 'react';
+/* global FB, location */
+import React, { Component, PropTypes } from 'react';
 import FaFacebookSquare from 'react-icons/lib/fa/facebook-square';
 import FaTwitter from 'react-icons/lib/fa/twitter';
 import styles from './SocialBtns.sass';
+import fbSdk from './fb_sdk';
 
-const SocialBtns = ({
-  fbLink = 'https://www.facebook.com',
-  twitterLink = `https://twitter.com/intent/tweet?text=${location.href}`,
-  iconSize = '25'
-}) => {
-  return (
-    <div className={styles.socialBtns}>
-      <a
-        href={fbLink}
-        className={styles.facebookBtn}
-      >
-        <FaFacebookSquare size={iconSize} />
-        <span>Share on Facebook</span
-      >
-      </a>
-      <a href={twitterLink} className={styles.twitterBtn}>
-        <FaTwitter size={iconSize} />
-        <span>Share on Twitter</span>
-      </a>
-    </div>
-  );
-};
+class SocialBtns extends Component {
+
+  componentDidMount() {
+    fbSdk.init();
+  }
+
+
+  render() {
+    const eventLink = location.href;
+    const twitterLink = `https://twitter.com/intent/tweet?text=${eventLink}`;
+    const iconSize = '25';
+
+    function fbShareHandler(event) {
+      event.preventDefault();
+      FB.ui({
+        method: 'share',
+        href: location.href,
+      });
+    }
+
+    return (
+      <div className={styles.socialBtns}>
+        <a
+          href={eventLink}
+          className={styles.facebookBtn}
+          onClick={fbShareHandler.bind(this)}
+        >
+          <FaFacebookSquare size={iconSize} />
+          <span>Share on Facebook</span
+        >
+        </a>
+        <a href={twitterLink} className={styles.twitterBtn}>
+          <FaTwitter size={iconSize} />
+          <span>Share on Twitter</span>
+        </a>
+      </div>
+    );
+  }
+
+}
 
 SocialBtns.propTypes = {
   fbLink: PropTypes.string,
