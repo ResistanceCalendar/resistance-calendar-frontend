858a2afba57d1b5f9c1a27d7de1e29191261ddf3
aaghevli <arash.aghevli@gmail.com>
2017-04-09 15:20:30 -0700

    Use location, not loc

--- src/api/eventsAPI.js
+++ src/api/eventsAPI.js
@@ -30,7 +30,7 @@ const temp = {
         "title": "Take Back Congress 2018",
         "description": "Our best chance to stop President Trump, and his radical agenda, is to organize early and elect a Democratic Congress in 2018.\n\nYoung voters, liberals\/progressives, and Democrats tend to skip elections in non-presidential years. That has to stop!\n\nHere's how you can help:\n\n1) RSVP \"Going\" for a reminder to vote on Election Day 2018, and\/or add this to your calendar by clicking http:\/\/evt.mx\/oy0WB9A7. \n\n2) Register to vote at https:\/\/turbovote.org\/ or https:\/\/www.iwillvote.com\/learn\/.\n\n3) Share this event with your friends. \n\n4) Follow our Facebook page Take Back Congress 2018.\n\nThe hosts: Brett and Alli met in college. Brett is a writer and campaign veteran. Alli's an artist. Basically they're two friends who think Trump's ideas, cabinet appointments, and tweets are completely bonkers and dangerous. Let's do something about it.",
         "start_date": "2018-11-06T06:01:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.756Z",
           "modified_date": "2017-03-21T01:53:56.756Z",
           "origin_system": "Facebook",
@@ -62,7 +62,7 @@ const temp = {
         "title": "Protest at Corrections Corporation of America: End Mass Incarceration",
         "description": "Corrections Corporation of America is one of the largest for profit prison companies making millions off the Mass Incarceration of Blacks and the Poor through lobbying, direct campaign contributions and increasing their influence politically. CCA was quoted in their 2014 Annual Report: \"The demand for our facilities and services could be adversely affected by the relaxation of enforcement efforts, leniency in conviction or parole standards and sentencing practices or through the decriminalization of certain activities that are currently proscribed by our criminal laws. For instance, any changes with respect to drugs and controlled substances or illegal immigration could affect the number of persons arrested, convicted, and sentenced, thereby potentially reducing demand for correctional facilities to house them.\" Do not allow CCA to profit from Mass Incarceration. Join us in Protesting CCA's private industry for profit \"slavery\". \n\nUPDATE: The Corrections Corporation of America, GEO Group, and the Avalon Corporation all have facilities in the Austin, Texas area and Wells Fargo, Bank of America, Barclays Intl. and Merrlin Lynch have invested in private prisons. In honor of the Robert Sobukwe who lead a demonstration to a Soweto, South Africa police station on March 21, 1960 and those who lost their lives protesting apartheid and police brutality at the Sharpeville Massacre on March 21, 1960 at a Sharpeville police station, we will continue to uphold the date of March 21. We will make this a mass protest with organizations and advocates working to dismantle the prison industrial complex in the fields of mental health, rehabilitation, recidivsm, and criminal justice. \n\nFURTHER UPDATE: To avoid displacement of the residents of the Austin Transitional Center, 3154 East Highway 71, Austin, Texas 78617 we have moved this event to next year 2018 back to the GEO Group's new headquarters 1777 NE Loop 410, San Antonio, Texas 78217.",
         "start_date": "2018-03-21T17:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.760Z",
           "modified_date": "2017-03-21T01:53:56.760Z",
           "origin_system": "Facebook",
@@ -112,7 +112,7 @@ const temp = {
         "title": "NC Votes 2017",
         "description": "\"What can I do?\"  \n\nSince the election, that's the question everyone's been asking - especially in North Carolina, where we've recently seen seen rampant abuse of power by the GOP-dominated state legislature (see http:\/\/www.nytimes.com\/2016\/12\/14\/us\/politics\/north-carolina-governor-roy-cooper-republicans.html?_r=0)..\n\nThis event is a simple way to make a BIG difference. It exists solely to remind people that North Carolina is having a special election in 2017 for many legislative districts across the state.  \n\nWhy are we having a special election in 2017?  Because a federal court has ordered us to redraw many of our districts due to the unlawful use of race in the last round of redistricting (see http:\/\/www.newsobserver.com\/news\/politics-government\/state-politics\/article117843388.html).  And new districts means new elections.\n\nHere's the big opportunity: If we can pick up a small handful of seats, we can break the GOP supermajority and sustain Gov. Cooper's vetoes.  Unlocking Gov. Cooper's veto would bring an abrupt halt to the far-right freight train that keeps pulling our state into the national news in ways that don't make us look good.\n\nThe key to winning those races is going to be getting out the vote, and this event is a simple tool to help do that.\n\nHere's all you need to do:\n\n1) Join our event.\n\n2) Share the event on your page (and re-share on your page whenever you think your friends need a reminder).\n\n3) ***Most Important*** - Invite your friends to join the event by clicking \"invite friends\" on the right-hand side of the event.  You can invite up to 500 friends.  Even if they don't join, they'll see the name of the event and that will tell them there's an election in 2017.\n\nThat's it!  Then, once you're done, check out our 2017 Gameplan at http:\/\/www.jeffjacksonnc.com\/gameplan-2017\/ for more easy, effective ways to lend a hand.",
         "start_date": "2017-11-07T11:30:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.761Z",
           "modified_date": "2017-03-21T01:53:56.761Z",
           "origin_system": "Facebook",
@@ -159,7 +159,7 @@ const temp = {
         "title": "A Book of Acts: Engaging Faith Communities in Black Lives Matter",
         "description": "#BlackLivesMatter  Update: This event is being rescheduled to coincide with Malice Green's tragic killing, which happens to be on a Sunday this year of 2017. Triumphant Love Lutheran Church is currently embarking on racism series January 8, 2017 - February 12, 2017 at 10:10 AM in the Fellowship Hall. You're welcome to attend in lieu of the above event being rescheduled. \n\nMalice Green was a resident of Detroit, Michigan who died after being assaulted by Detroit police officers Walter Budzyn and Larry Nevers on November 5, 1992 (pictured above). On this day join Black Lives Matter for a training on engaging faith communities in social justice. \"Everyone is welcome at the table\". Food and childcare will be provided.",
         "start_date": "2017-11-05T18:30:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.764Z",
           "modified_date": "2017-03-21T01:53:56.764Z",
           "origin_system": "Facebook",
@@ -209,7 +209,7 @@ const temp = {
         "title": "NDP Fall Meeting",
         "description": "The Nebraska Democratic Party has quarterly meetings where Caucuses, Committees and the full State Central Committee (SCC) meet for trainings, education and to conduct party business.\n\nAll Democrats and Independents are welcome to attend. Only SCC members can vote during the SCC meeting. You can contact your county party chair to see if there is an opening in your area to represent your community on the SCC.\n\nYou do not have to be an SCC member to attend the meetings and participate in the Caucus meetings (like Women\u2019s, LGBT, Native, etc).\n\nThe agenda will be posted in September. But, SAVE THE DATE!",
         "start_date": "2017-10-21T13:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.765Z",
           "modified_date": "2017-03-21T01:53:56.765Z",
           "origin_system": "Facebook",
@@ -259,7 +259,7 @@ const temp = {
         "title": "Week Without Violence",
         "description": "Each year, YWCA USA, YWCA local associations around the country, friends, partners, and supporters come together in the third week of October for a Week Without Violence, where we raise awareness about the devastating effects of domestic violence. As the largest network of domestic violence service providers, we support victims of domestic violence in communities around the country and work, year-round, on legislative actions and issue education that can bring an end to all forms of violence against women.",
         "start_date": "2017-10-16T04:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.767Z",
           "modified_date": "2017-03-21T01:53:56.767Z",
           "origin_system": "Facebook",
@@ -295,7 +295,7 @@ const temp = {
         "title": "Women's March on London-TBA-Official State Visit",
         "description": "On January 27th Theresa May announced that Donald Trump will make an official state visit to Britain later in 2017.\n\nWATCH THIS SPACE. More information will be available soon. \n\nTime and date still to be announced.\n\nhttps:\/\/www.facebook.com\/womensmarchlondon\/\n\nhttps:\/\/www.facebook.com\/groups\/1332581573479599\/\n\nwomensmarchlondon.com",
         "start_date": "2017-10-05T20:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.768Z",
           "modified_date": "2017-03-21T01:53:56.768Z",
           "origin_system": "Facebook",
@@ -329,7 +329,7 @@ const temp = {
         "title": "Stand Up To Trump: Protest his visit to Britain!",
         "description": "PLEASE NOTE: The provisional dates now announced, of 5th to 8th October 2017. Get ready for massive protests. \n\nProtest called by:\nStand Up to Racism\nStop the War Coalition \nPeople's Assemby Against Austerity\nCND\n\nTheresa May and Donald Trump have just announced Trump will be visiting the UK later this year\n\nWhen the date is announced, we will be organising protests to oppose Trump's racism, sexism and bigotry.\n\nThe invitation to Donald Trump for a state visit will be opposed by millions in Britain. Our government should not be seen to be endorsing the sorts of ideas and policies he is putting forward. We are committed, along with other campaigning organisations including Stop the War, Peoples Assembly against Austerity and CND , to oppose this visit and to organise mass protests if it takes place. \n\nJoin our protest to tell Trump his racism and bigotry isn't welcome in the UK\n\nMore details TBC",
         "start_date": "2017-10-05T11:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.768Z",
           "modified_date": "2017-03-21T01:53:56.768Z",
           "origin_system": "Facebook",
@@ -363,7 +363,7 @@ const temp = {
         "title": "Indiana NOW State Conference",
         "description": "Save the Date! \n\nInterested in helping us plan this event? Let us know!",
         "start_date": "2017-09-30T13:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.769Z",
           "modified_date": "2017-03-21T01:53:56.769Z",
           "origin_system": "Facebook",
@@ -427,7 +427,7 @@ const temp = {
         "title": "Bernie Sanders in Montreal",
         "description": "Nous avons cr\u00e9\u00e9 cette page afin de d\u00e9montrer \u00e0 l'\u00e9quipe de Bernier Sanders que nous voulons recevoir le S\u00e9nateur ici \u00e0 Montreal! \n\nLe mouvement continue et on a besoin de votre appui, alors allez appuyer sur le bouton ''GOING'' et PARTAGEZ l`\u00e9v\u00e9nement avec vos amis!\n\nSi vous avez des id\u00e9es et vous avez envie de participer en tant que b\u00e9n\u00e9vole ou de n'importe quelle mani\u00e8re, n'h\u00e9sitez pas \u00e0 communiquer avec nous \u00e0 yprincebidounga@cirqc.org.\n\n__________________________________________________________\n\nThis event was created as a mean to measure the interest for a visit of Bernie Sanders in Montreal.\n\nWe will use the interest for this page, to convince Senator Sanders staff to help us in making this historical event  happen. \n\nIf you would like to participate in this effort, as a volunteer, feel free to write to me at yprincebidounga@cirqc.org",
         "start_date": "2017-09-01T22:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.770Z",
           "modified_date": "2017-03-21T01:53:56.770Z",
           "origin_system": "Facebook",
@@ -463,7 +463,7 @@ const temp = {
         "title": "She Persisted March and Rally Los Angeles by Project Femme",
         "description": "Placed on National Equality Day, march with us in support of all women, nonbinary babes, immigrants, refugees, POC, and every minority who is persisting through this tough political climate\u2764\ufe0f\n\nps! please share this event as far as you can\n\nthis march is hosted by project femme, a youth-run organization that aims to uplift the voices of female, nonbinary, and LGBTQ+ youth through social activism and running for public office.",
         "start_date": "2017-08-26T19:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.771Z",
           "modified_date": "2017-03-21T01:53:56.771Z",
           "origin_system": "Facebook",
@@ -513,7 +513,7 @@ const temp = {
         "title": "Akron Pride 2017",
         "description": "Come and celebrate the first annual Akron LGBTQ Pride. Food trucks, vendors, local talent, and a national talent to be announced! Come share the love and pride of Akron! \nThe website should be up soon with links for volunteering and sponsorship. Thanks to everyone for your willingness to help and participate!",
         "start_date": "2017-08-26T16:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.772Z",
           "modified_date": "2017-03-21T01:53:56.772Z",
           "origin_system": "Facebook",
@@ -563,7 +563,7 @@ const temp = {
         "title": "Racial Justice Institute",
         "description": "On the first full day of the Embody Progress Conference, we will present a Day-Long Racial Justice Institute. Come learn about systemic oppression, intersectionality, and racial justice. \n\nEarly Bird Registration is $35 and will increase to $50 after April 1. \n\nThe cost of the Racial Justice Institute is included in the full registration of the Embody Progress Conference!",
         "start_date": "2017-08-24T14:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.775Z",
           "modified_date": "2017-03-21T01:53:56.775Z",
           "origin_system": "Facebook",
@@ -599,7 +599,7 @@ const temp = {
         "title": "Embody Progress Conference",
         "description": "The Embody Progress Conference will act as a catalyst for unifying organizers, funders, and thought leaders on a national scale with grassroot community organizers in hopes to mobilize around issues facing LGBTQ people on local, regional, and national levels.\n\nPurpose:\nGather LGBTQ people, organizers, educators, and health administrators nationwide to\nReclaim Southern spaces in the name of raising\nOur voices, our bodies, and our strengths as\nWe seek to further our knowledge, resources, and networks\nTo bring back into our local spaces and champion the\nHeritage of the shoulders we stand on.",
         "start_date": "2017-08-24T13:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.776Z",
           "modified_date": "2017-03-21T01:53:56.776Z",
           "origin_system": "Facebook",
@@ -649,7 +649,7 @@ const temp = {
         "title": "Oppose the Hate at the 2017 American Renaissance Conference!",
         "description": "It was first announced on the neo-Nazi website Stormfront although it isn't being promoted heavily on the American Renaissance website itself, but considering the fact that the Age of Trump has meant the neo-Fascists in this country are being confronted by those who do not want those ideals running this society, they know that AmRen 2017 is going to be met with opposition. That's probably why the theme of this year's conference is \"Turning the Tide\", That's not just a reference to what they think might be happening during Trump's reign. It is also the name of a popular antifa publication by Anti-Racist Action L.A.-People Against Racist Terror! That's a good sign that they are picking a fight, but this is the year where all realize that we do indeed need to come out and protect our freedoms like never before. Montgomery Bell Park Inn just might find itself with the biggest anti-AmRen rally they have seen since AmRen had been hosting conferences there in 2013.\n\nThe American Renaissance Conference - AmRen for short - is sponsored by the newsletter published by White Supremacist Jared Taylor, considered the godfather of the so-called 'alt-right\". It will be held at  Montgomery Bell State Park Inn, 45 minutes from Nashville, Tennessee and there is no public transportation there. They chose this location not just because it is a public facility - which means by the way that the public has a right to be inside the conference center that we should say has a great restaurant - but because it is so remote. That's not going to deter folks this time around, and the fact that this is taking place in the summer as opposed to the usual spring outings they have, expect that even more.",
         "start_date": "2017-07-28T22:00:00.000Z",
-        "loc": {
+        "location": {
           "created_date": "2017-03-21T01:53:56.780Z",
           "modified_date": "2017-03-21T01:53:56.780Z",
           "origin_system": "Facebook",
