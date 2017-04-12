c50f6265f3133293ae8ab7be865b7259a14599b1
sdotson <stu.dotson@gmail.com>
2017-04-11 21:48:54 -0400

    use window.location, remove linter override comment

--- src/components/SocialBtns/SocialBtns.js
+++ src/components/SocialBtns/SocialBtns.js
@@ -1,12 +1,11 @@
-/* global FB, location */
 import React, { PropTypes } from 'react';
 import FaFacebookSquare from 'react-icons/lib/fa/facebook-square';
 import FaTwitter from 'react-icons/lib/fa/twitter';
 import styles from './SocialBtns.sass';
 
 const SocialBtns = ({
-  fbLink = `https://www.facebook.com/sharer/sharer.php?u=${location.href}`,
-  twitterLink = `https://twitter.com/intent/tweet?text=${location.href}`,
+  fbLink = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
+  twitterLink = `https://twitter.com/intent/tweet?text=${window.location.href}`,
   iconSize = '25'
 }) => {
   return (
