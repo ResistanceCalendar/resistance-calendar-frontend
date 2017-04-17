000452da43f2a36b556d9b49fa85fb5db8c42d0a
pfarnach <pfarnac1@gmail.com>
2017-04-17 18:42:16 +0200

    Crops event card thumbnail image

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -18,6 +18,15 @@ function renderLocation(location) {
   return null;
 }
 
+function getCroppedImageUrl(url) {
+  // See: http://cloudinary.com/documentation/image_transformation_reference
+  // example URL: https://res.cloudinary.com/hqrdtqlz0/image/upload/c_thumb,g_faces:center,z_0.75,h_80,w_80/facebook:162777260891158
+  const separator = '/image/upload';
+  const [url1, url2] = url.split(separator);
+
+  return `${url1}${separator}/c_thumb,g_faces:center,z_0.75,h_200,w_200/${url2}`;
+}
+
 const EventCard = ({ event, className }) => {
   const {
     start_date: startDate,
@@ -28,14 +37,15 @@ const EventCard = ({ event, className }) => {
     location
   } = event;
 
-  // TODO: Seems like the featured_image_url is no longer coming back -- follow up when services more stable
+  // TODO: May have to change how this check is done once we switch over to real default image
+  const croppedFeaturedImageUrl = featuredImageUrl ? getCroppedImageUrl(featuredImageUrl) : 'http://www.fillmurray.com/550/450';
 
   return (
     <li className={`${styles.card} ${className || ''}`}>
       <Link to={`/event/${_id}`}>
         <div className={styles.imageWrapper}>
           <img
-            src={featuredImageUrl || 'http://www.fillmurray.com/550/450'}
+            src={croppedFeaturedImageUrl}
             alt="featured event"
           />
         </div>
