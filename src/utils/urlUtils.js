ed28cd264197874017e2b118a7d589a52c60cfcc
pfarnach <pfarnac1@gmail.com>
2017-05-08 14:03:25 +0200

    Sets max-width on cloudinary event details image

---
+++ src/utils/urlUtils.js
@@ -0,0 +1,12 @@
+function getImageUrl(url, imageFormat) {
+  // See: http://cloudinary.com/documentation/image_transformation_reference
+  // example URL: https://res.cloudinary.com/hqrdtqlz0/image/upload/c_thumb,g_faces:center,z_0.75,h_80,w_80/facebook:162777260891158
+  const separator = '/image/upload';
+  const [url1, url2] = url.split(separator);
+
+  return `${url1}${separator}/${imageFormat}/${url2}`;
+}
+
+export default {
+  getImageUrl
+};
