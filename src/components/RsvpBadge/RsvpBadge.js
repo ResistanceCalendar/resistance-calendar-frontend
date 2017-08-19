a2a01573919f04c98c1a1bf6a7c9bac93fe6a57f
Len Smith <len@barrison.com>
2017-08-19 15:14:00 -0700

    Added test and formatted > 1K RSVPs

--- src/components/RsvpBadge/RsvpBadge.js
+++ src/components/RsvpBadge/RsvpBadge.js
@@ -1,11 +1,15 @@
 import React, { PropTypes } from 'react';
 import styles from './RsvpBadge.sass';
 
+const toK = (n) => {
+  return n >= 1000 ? (n / 1000).toFixed(1) + 'K' : n;
+};
+
 const RsvpBadge = ({ totalAccepted }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.left}>
-        {totalAccepted}
+        {toK(totalAccepted)}
       </div>
       <div className={styles.right}>
         RSVPS
