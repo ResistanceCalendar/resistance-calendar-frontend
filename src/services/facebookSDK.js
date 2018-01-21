b43959018b2e46678e3c4bc2322ae73b591ce4a2
Doron Orenstein <jazziseverywhere@gmail.com>
2018-01-20 20:36:32 -0600

    got it working w/ngrok, this will probably work though

--- src/services/facebookSDK.js
+++ src/services/facebookSDK.js
@@ -3,30 +3,38 @@ import { facebookAppId } from '../config';
 
 export default {
   init() {
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : facebookAppId,
-        xfbml      : true,
-        version    : 'v2.9'
-      });
-      FB.AppEvents.logPageView();
-    };
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : facebookAppId,
+          xfbml      : true,
+          version    : 'v2.11'
+        });
+        FB.AppEvents.logPageView();
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = 'https://connect.facebook.net/en_US/sdk.js';
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
   },
   share({ title, eventDate, picture, eventUrl, description }) {
     FB.ui({
-      method: 'share',
-      href: eventUrl,
-      caption: eventDate,
-      title,
-      description,
-      picture
+      method: 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+        object: {
+          'fb:app_id': facebookAppId,
+          'og:url': eventUrl,
+          'og:title': title,
+          'og:description': description,
+          'og:image': picture,
+          'og:image:width': '600',
+          'og:image:height': '350'
+        }
+      })
     });
   }
 };
